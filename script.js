3c119d41b59f41d2567aaa963299ce544c2e5e38
Amit Kumar <83487500+amitkrxyz@users.noreply.github.com>
2022-06-24 13:57:52 +0530

    fix but when key is held down

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let justPlayed = null;
 const playSound = (key) => {
+  if (key === justPlayed) return;
   const keyElement = document.querySelector(`div[data-key="${key}"]`);
   let audio = document.querySelector(`audio[data-key="${key}"]`);
   if (keyElement != null) {
@@ -11,15 +13,21 @@ const playSound = (key) => {
   }
   audio.currentTime = 0;
   audio.play();
+  justPlayed = key;
 };
 
-window.addEventListener("keydown", (e) => {
-  playSound(e.key);
-});
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   e.target.classList.remove("playing");
 }
+
+window.addEventListener("keydown", (e) => {
+  playSound(e.key);
+});
+window.addEventListener("keyup", () => {
+  justPlayed = null;
+});
+
 const keys = Array.from(document.querySelectorAll(".key"));
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("click", (e) => {
@@ -29,10 +37,13 @@ window.addEventListener("click", (e) => {
   ) {
     playSound(null);
   } else {
-    keys.forEach((key) =>
+    keys.forEach((key) => {
       key.addEventListener("pointerdown", (e) => {
         playSound(e.currentTarget.dataset.key);
-      })
-    );
+      });
+      key.addEventListener("pointerup", (e) => {
+        justPlayed = null;
+      });
+    });
   }
 });
